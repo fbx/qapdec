bf991dd9f66c916d2ee88858b8651eef6a912fed
Arnaud Vrac <avrac@freebox.fr>
2019-12-18 20:26:46 +0100

    change dolby library name to match reference build

--- qapdec.c
+++ qapdec.c
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef QAP_LIB_DOLBY_MS12
-# define QAP_LIB_DOLBY_MS12 "/usr/lib64/libdolby_ms12_wrapper.so"
+# define QAP_LIB_DOLBY_MS12 "/usr/lib64/libdolby_ms12_wrapper_prod.so"
 #endif
 
 #define AUDIO_OUTPUT_ID_BASE 0x100
